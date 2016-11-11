52dc3a821d8d0b582aac7657077dcf8695fb5bfe
Shinzo SAITO <architshin@websarva.com>
2016-11-11 22:15:26 +0900

    Chap03に「FourArithmeticOperations.java」の追加。

---
+++ Chap03/src/FourArithmeticOperations.java
@@ -0,0 +1,18 @@
+public class FourArithmeticOperations {
+
+	public static void main(String[] args) {
+		int num1 = 6;
+		int num2 = 3;
+
+		int ans1 = num1 + num2;  // (1)
+		int ans2 = num1 - num2;  // (2)
+		int ans3 = num1 * num2;  // (3)
+		int ans4 = num1 / num2;  // (4)
+
+		System.out.println(ans1);
+		System.out.println(ans2);
+		System.out.println(ans3);
+		System.out.println(ans4);
+	}
+
+}
