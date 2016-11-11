ba86e041843ef1d67d67e0242bb073bcb21ca34d
Shinzo SAITO <architshin@websarva.com>
2016-11-11 20:42:12 +0900

    Chap03の「UseVariables.java」の追加。

---
+++ Chap03/src/UseVariables.java
@@ -0,0 +1,17 @@
+public class UseVariables {
+
+	public static void main(String[] args) {
+		String name;  // (1)
+		int num;  // (2)
+		double dec;  // (3)
+
+		name = "佐藤";  // (4)
+		num = 551;  // (5)
+		dec = 67.8;  // (6)
+
+		System.out.println(name);
+		System.out.println(num);
+		System.out.println(dec);
+	}
+
+}
