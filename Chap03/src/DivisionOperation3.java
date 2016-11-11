fb976c3081b4345f2b2d1da59ac5d7e50f0a0fd6
Shinzo SAITO <architshin@websarva.com>
2016-11-11 22:45:24 +0900

    Chap03に「DivisionOperation1〜4」を追加。

---
+++ Chap03/src/DivisionOperation3.java
@@ -0,0 +1,12 @@
+public class DivisionOperation3 {
+
+	public static void main(String[] args) {
+		double num1 = 6;
+		double num2 = 5;
+
+		double ans = num1 / num2;
+
+		System.out.println(ans);
+	}
+
+}
---
+++ Chap03/src/DivisionOperation4.java
@@ -0,0 +1,12 @@
+public class DivisionOperation4 {
+
+	public static void main(String[] args) {
+		int num1 = 6;
+		int num2 = 5;
+
+		double ans = (double)num1 / num2;
+
+		System.out.println(ans);
+	}
+
+}
