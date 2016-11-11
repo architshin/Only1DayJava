e04fa8a986cf1e2700557bd03cf16dd68a04cc2e
Shinzo SAITO <architshin@websarva.com>
2016-11-11 22:03:47 +0900

    chap03に「CalcVariablePlus.java」の追加。

---
+++ Chap03/src/CalcVariablePlus.java
@@ -0,0 +1,10 @@
+public class CalcVariablePlus {
+
+	public static void main(String[] args) {
+		int num1 = 478;  // (1)
+		int num2 = 914;  // (2)
+		int ans = num1 + num2;  // (3)
+		System.out.println(ans);
+	}
+
+}
