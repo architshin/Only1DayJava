a21581a26926ad5e6312f42fc3b11f7bbc0fd0b3
Shinzo SAITO <architshin@websarva.com>
2016-11-14 21:54:58 +0900

    Chap03に「IncrementAndDecrement.java」を追加。

---
+++ Chap03/src/IncrementAndDecrement.java
@@ -0,0 +1,9 @@
+public class IncrementAndDecrement {
+	public static void main(String[] args) {
+		int num = 100;
+		num++;  // (1)
+		System.out.println("numの値: " + num);
+		num--;  // (2)
+		System.out.println("numの値: " + num);
+	}
+}
