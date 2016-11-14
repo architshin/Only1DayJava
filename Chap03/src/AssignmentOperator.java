34f73cd0b03e5f4c6aac2297ea2dbf6d477ec8d6
Shinzo SAITO <architshin@websarva.com>
2016-11-14 19:29:40 +0900

    Chap03に「AssignmentOperator.java」を追加。

---
+++ Chap03/src/AssignmentOperator.java
@@ -0,0 +1,12 @@
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int num = 346;
+		num = num + 4;  // (1)
+		System.out.println("numの値: " + num);
+
+		num += 5;  // (2)
+		System.out.println("numの値: " + num);
+	}
+
+}
