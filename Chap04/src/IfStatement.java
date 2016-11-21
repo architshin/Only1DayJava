1d8e53d13cf6e800fe5c9c589f4889277dd465b0
Shinzo SAITO <architshin@websarva.com>
2016-11-21 21:03:14 +0900

    Chap04プロジェクト、及び、IfStatement.javaの追加。

---
+++ Chap04/src/IfStatement.java
@@ -0,0 +1,9 @@
+public class IfStatement {
+	public static void main(String[] args) {
+		int num = 30;
+		if(num <= 40) {
+			System.out.println("数値は40以下です。");
+		}
+		System.out.println("処理終了!");
+	}
+}
