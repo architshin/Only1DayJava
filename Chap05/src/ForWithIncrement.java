d554d8a7a1ac994ce3247f63b3dc8861de4b471f
Shinzo SAITO <architshin@websarva.com>
2016-11-25 22:36:56 +0900

    Chap05に「ForWithIncrement.java」を追加。

---
+++ Chap05/src/ForWithIncrement.java
@@ -0,0 +1,9 @@
+public class ForWithIncrement {
+	public static void main(String[] args) {
+		int ans = 0;
+		for(int i = 1; i <= 100; i++) {
+			ans += i;
+		}
+		System.out.println("答え: " + ans);
+	}
+}
