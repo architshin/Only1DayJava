7f9c8557c6489e333e87c2b1dfb6aacf662011dd
Shinzo SAITO <architshin@websarva.com>
2016-11-27 16:59:31 +0900

    Chap05に「WhileAndDoWhileStatement.java」を追加。

---
+++ Chap05/src/WhileAndDoWhileStatement.java
@@ -0,0 +1,18 @@
+public class WhileAndDoWhileStatement {
+	public static void main(String[] args) {
+		System.out.println("whileループの開始");
+		int i = 1;
+		while(i < 0) {
+			System.out.println("i = " + i);
+		}
+		System.out.println("whileループ終了");
+
+		System.out.println("do-whileループの開始");
+		int j = 1;
+		do {
+			System.out.println("j = " + j);
+		}
+		while(j < 0);
+		System.out.println("do-whileループ終了");
+	}
+}
