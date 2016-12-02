0c0bd9290d158da9d26d1cf0d288a7131e0108f1
Shinzo SAITO <architshin@websarva.com>
2016-12-02 22:47:29 +0900

    Chap06に「UseLength.java」を追加。

---
+++ Chap06/src/UseLength.java
@@ -0,0 +1,9 @@
+public class UseLength {
+	public static void main(String[] args) {
+		int[] nums = {8, 9, 17, 26, 43};
+
+		for(int i = 0; i < nums.length; i++) {
+			System.out.println((i+1) + "個目: " + nums[i]);
+		}
+	}
+}
