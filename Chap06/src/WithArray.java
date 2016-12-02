4990c77a7d187797674b6419cf3c4b532066cfb4
Shinzo SAITO <architshin@websarva.com>
2016-12-02 15:22:14 +0900

    Chap06に「WithArray.java」を追加。

---
+++ Chap06/src/WithArray.java
@@ -0,0 +1,17 @@
+public class WithArray {
+	public static void main(String[] args) {
+		int[] nums = new int[5];
+
+		nums[0] = (int) (10 * Math.random()) + 1;
+		nums[1] = nums[0] + 1;
+		nums[2] = nums[0] + nums[1];
+		nums[3] = nums[1] + nums[2];
+		nums[4] = nums[2] + nums[3];
+
+		System.out.println("1個目: " + nums[0]);
+		System.out.println("2個目: " + nums[1]);
+		System.out.println("3個目: " + nums[2]);
+		System.out.println("4個目: " + nums[3]);
+		System.out.println("5個目: " + nums[4]);
+	}
+}
