f700cbf6d71b3ac7540b86404d42ab176f01f55f
Shinzo SAITO <architshin@websarva.com>
2016-12-03 17:15:01 +0900

    Chap06に「EnhancedForStatement.java」を追加。

---
+++ Chap06/src/EnhancedForStatement.java
@@ -0,0 +1,9 @@
+public class EnhancedForStatement {
+	public static void main(String[] args) {
+		int[] nums = {8, 9, 17, 26, 43};
+
+		for(int num : nums) {
+			System.out.println(num);
+		}
+	}
+}
