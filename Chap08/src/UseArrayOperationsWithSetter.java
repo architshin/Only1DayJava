17900a93db996d6b440a62ca66aa6d0d0d7f8b16
Shinzo SAITO <architshin@websarva.com>
2016-12-19 17:07:24 +0900

    Chap08プロジェクト、及び、ArrayOperationsWithSetter.javaとUseArrayOperationsWithSetter.javaの追加。

---
+++ Chap08/src/UseArrayOperationsWithSetter.java
@@ -0,0 +1,11 @@
+public class UseArrayOperationsWithSetter {
+	public static void main(String[] args) {
+		int[] list = {4, 5, 84, 7, 1, 15};
+
+		ArrayOperationsWithSetter arrayOpe = new ArrayOperationsWithSetter();
+		arrayOpe.setArray(list);
+		int sum = arrayOpe.getSumArray();
+		double ave = arrayOpe.getAverage();
+		System.out.println("配列内の合計: " + sum + " 平均: " + ave);
+	}
+}
