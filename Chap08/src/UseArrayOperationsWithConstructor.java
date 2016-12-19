31720acb473088d036720a1cadee9d9d26b00826
Shinzo SAITO <architshin@websarva.com>
2016-12-19 18:47:18 +0900

    Chap08に「ArrayOperationsWithConstructor.java」と「UseArrayOperationsWithConstructor.java」を追加。

---
+++ Chap08/src/UseArrayOperationsWithConstructor.java
@@ -0,0 +1,10 @@
+public class UseArrayOperationsWithConstructor {
+	public static void main(String[] args) {
+		int[] list = {4, 5, 84, 7, 1, 15};
+
+		ArrayOperationsWithConstructor arrayOpe = new ArrayOperationsWithConstructor(list);
+		int sum = arrayOpe.getSumArray();
+		double ave = arrayOpe.getAverage();
+		System.out.println("配列内の合計: " + sum + " 平均: " + ave);
+	}
+}
